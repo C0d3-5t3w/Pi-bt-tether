9423925422d2b27839a4b9314554d6a31115faab
5T3W <stewy1994@gmail.com>
2025-03-03 13:56:46 +0000

    Update bt-tether.sh

--- bt-tether.sh
+++ bt-tether.sh
@@ -19,7 +19,7 @@ fi
 # Prompt user for input
 read -p "Enter Bluetooth device MAC (e.g., A1:11:1B:C1:D1:E1): " -r BT_DEVICE
 read -p "Enter phone connection name (e.g., 5T3W): " -r CONNECTION_NAME
-read -p "Enter static ip (e.g., 172.20.10.6 for ios 192.168.44.1 for andriod): " -r STATIC_IP
+read -p "Enter static ip (e.g., 172.20.10.1 for ios 192.168.44.1 for andriod): " -r STATIC_IP
 read -p "Enter gateway (e.g., 255.255.255.0): " -r GATEWAY_IP
 
 # Execute nmcli with user input
